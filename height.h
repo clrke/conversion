c1de4b21135a7710a5562797336dad024efb9543
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-01-05 16:01:47 +0800

    Fix conversion for height

--- height.h
+++ height.h
@@ -1,44 +1,68 @@
-#define fahrenheit 1
-#define celsius 2
-#define kelvin 3
+#define cm 1
+#define inch 2
+#define foot 3
 
 float convert_height(int convert_type, float height) {
 	switch(convert_type) {
-		case fahrenheit:
-			return height*1.8f + 32;
-		case celsius:
-			return (height-32) / 1.8f;
-		case kelvin:
-			return ((height-32) / 1.8f) + 273.15;
-		default:
-			return 0;
+		case 1:
+			return height/2.54;
+		case 2:
+			return height/30.48;
+		case 3:
+			return height/12;
+		case 4:
+			return height*2.54;
+		case 5:
+			return height*30.48;
+		case 6:
+			return height*12;
 	}
 }
 
 void height_conversion() {
 	printf("Height Conversion\n");
-	printf("[1] Celsius to Fahrenheit\n");
-	printf("[2] Fahrenheit to Celsius\n");
-	printf("[3] Fahrenheit to Kelvin\n");
+
+	printf("[1] cm to inch\n");
+	printf("[2] cm to foot\n");
+	printf("[3] inch to foot\n");
+	printf("[4] inch to cm\n");
+	printf("[5] foot to cm\n");
+	printf("[6] foot to inch\n");
 
 	printf(">>> ");
 	int convert_type;
 	scanf("%d", &convert_type);
 
-	if(convert_type == 1 || convert_type == 2 || convert_type == 3) {
-		printf("%s: ", convert_type != fahrenheit ? "Fahrenheit" : "Celsius"); // ternary
+	if(convert_type >= 1 && convert_type <= 6) {
+		switch(convert_type) {
+			case 1:
+			case 2:
+				printf("cm: ");
+				break;
+			case 3:
+			case 4:
+				printf("inch: ");
+				break;
+			case 5:
+			case 6:
+				printf("foot: ");
+				break;
+		}
 		float height;
 		scanf("%f", &height);
 
 		switch(convert_type) {
-			case fahrenheit:
-				printf("Fahrenheit");
+			case 6:
+			case 1:
+				printf("inch");
 				break;
-			case celsius:
-				printf("Celsius");
+			case 2:
+			case 3:
+				printf("foot");
 				break;
-			case kelvin:
-				printf("Kelvin");
+			case 4:
+			case 5:
+				printf("cm");
 				break;
 		}
 		printf(": %.2f\n", convert_height(convert_type, height));
